b447608fcd98ebff2ea883f2d3407e40d417385b
Oussama <Messamessus@yahoo.com>
2025-04-21 15:10:23 +0100

    set url index

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
   : [
     "http://localhost:5173",
+    "https://original-project-app-frontend.vercel.app",
       
       
     ];
